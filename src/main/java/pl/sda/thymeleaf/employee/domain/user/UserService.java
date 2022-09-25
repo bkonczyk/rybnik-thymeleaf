c951907727fcdc02b897f04b96ef7fcd0d8a435d
bkonczyk <bartkon@gmail.com>
2022-09-25 12:21:57 +0200

    implementing user security

--- src/main/java/pl/sda/thymeleaf/employee/domain/user/UserService.java
+++ src/main/java/pl/sda/thymeleaf/employee/domain/user/UserService.java
@@ -1,8 +1,11 @@
 package pl.sda.thymeleaf.employee.domain.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Supplier;
 
 @Service
 @RequiredArgsConstructor
@@ -17,9 +20,19 @@ public class UserService {
     }
 
     private User mapToUser(CreateUserRequest request) {
+        String username = request.getUsername();
+        if (repository.existsById(username)) {
+            throw new RuntimeException("Username " + username + " already exists");
+        }
         return User.of(
-                request.getUsername(),
+                username,
                 passwordEncoder.encode(request.getPassword()),
                 "USER");
+    }
+
+    public User findByUsername(String username) {
+        return repository
+                .findById(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username + " was not found"));
     }
 }
---
+++ src/main/java/pl/sda/thymeleaf/employee/security/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package pl.sda.thymeleaf.employee.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pl.sda.thymeleaf.employee.domain.user.User;
+import pl.sda.thymeleaf.employee.domain.user.UserService;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserService service;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = service.findByUsername(username);
+    }
+}
